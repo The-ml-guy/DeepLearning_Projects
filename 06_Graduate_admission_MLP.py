fb2ebb280071932c27d04af67f87ca0f903d0990
The ml guy <unknowninsaa1@gmail.com>
2026-09-06 01:13:25 +0530

    Create 06_Graduate_admission_MLP.py

---
+++ 06_Graduate_admission_MLP.py
@@ -0,0 +1,94 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+
+# Use the kagglehub client library to attach Kaggle resources like competitions, datasets, and models to your session
+# Learn more about kagglehub: https://github.com/Kaggle/kagglehub/blob/main/README.md
+
+import kagglehub
+# kagglehub.dataset_download('<owner>/<dataset-slug>')
+
+df = pd.read_csv("/kaggle/input/datasets/mohansacharya/graduate-admissions/Admission_Predict_Ver1.1.csv")
+
+df.drop(columns=['Serial No.'], inplace=True)
+
+df.info()
+
+df.duplicated() .sum()
+
+X = df.iloc[:,0:-1]
+y = df.iloc[:,-1]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+X_train_scaled
+
+import tensorflow
+from tensorflow import keras
+from keras import Sequential
+from keras.layers import Dense
+
+model = Sequential()
+
+model.add(Dense(7,activation='relu',input_dim=7))
+model.add(Dense(7,activation='relu'))
+model.add(Dense(1,activation='linear'))
+
+model.summary()
+
+model.compile(loss='mean_squared_error',optimizer='Adam')
+
+
+history = model.fit(X_train_scaled, y_train, epochs=100,validation_split=0.2)
+
+Epoch 1/100
+# 10/10 ━━━━━━━━━━━━━━━━━━━━ 0s 9ms/step - loss: 0.0124 - val_loss: 0.0112
+# Epoch 2/100
+# 10/10 ━━━━━━━━━━━━━━━━━━━━ 0s 6ms/step - loss: 0.0117 - val_loss: 0.0106
+# Epoch 3/100
+# 10/10 ━━━━━━━━━━━━━━━━━━━━ 0s 6ms/step - loss: 0.0112 - val_loss: 0.0102
+# Epoch 4/100
+# 10/10 ━━━━━━━━━━━━━━━━━━━━ 0s 6ms/step - loss: 0.0107 - val_loss: 0.0098
+# Epoch 5/100
+# 10/10 ━━━━━━━━━━━━━━━━━━━━ 0s 6ms/step - loss: 0.0102 - val_loss: 0.0094
+# Epoch 6/100
+# 10/10 ━━━━━━━━━━━━━━━━━━━━ 0s 6ms/step - loss: 0.0099 - val_loss: 0.0091
+# Epoch 7/100
+# 10/10 ━━━━━━━━━━━━━━━━━━━━ 0s 6ms/step - loss: 0.0095 - val_loss: 0.0088
+# Epoch 8/100
+# 10/10 ━━━━━━━━━━━━━━━━━━━━ 0s 6ms/step - loss: 0.0092 - val_loss: 0.0086
+# Epoch 9/100
+# 10/10 ━━━━━━━━━━━━━━━━━━━━ 0s 6ms/step - loss: 0.0090 - val_loss: 0.0083
+# Epoch 10/100
+# 10/10 ━━━━━━━━━━━━━━━━━━━━ 0s 5ms/step - loss: 0.0087 - val_loss: 0.0080
+
+
+
+
+y_pred = model.predict(X_test_scaled)
+
+from sklearn.metrics import r2_score
+r2_score(y_test, y_pred)
+
+#Score coming is 0.7676244550154735
